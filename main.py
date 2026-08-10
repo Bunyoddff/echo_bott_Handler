e08725ab6b658bacb12c9c0ec726b72e78cc48fe
1234 <a1234@MackBookpro.local>
2026-08-10 17:47:40 +0500

    hiding the token

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import telegram
-bot=telegram.Bot(token='8877209931:AAH6Bkhk9IfRQ53_00N7Ux3QEfr2p2YXb8M')
+from dotenv import loaddotenv
+import os
+loaddotenv()
+token=os.getenc('token')
+bot=telegram.Bot(token)
 update=bot.getUpdates()
 for i in 
 chat_i=update[-1].message.chat_id
