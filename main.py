8de65e8a6ccc513631cdabd403b6075fe360c6d3
1234 <a1234@MackBookpro.local>
2026-08-13 11:52:12 +0500

    full echo bot based on the requriments

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ def salom(update, context):
 def other(update, context):
     update.message.reply_text(update.message.text)
 
+def bye(update,context):
+    update.message.reply_text('Goodbye! Have great day!')
+
 hello_handler = MessageHandler(Filters.regex('hello'), salom)
 other_handler = MessageHandler(Filters.text & ~Filters.regex('hello'),other)
-
+bye_handler = MessageHandler(Filters.regex('bye'), bye)
 dispatcher.add_handler(hello_handler)
+dispatcher.add_handler(bye_handler)
 dispatcher.add_handler(other_handler)
 
 print("i am here now...")
